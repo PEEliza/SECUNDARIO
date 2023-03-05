036a454acd8671bf2894b3fc1ec1e789fdbe38ca
Poleth Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2023-03-04 18:40:41 -0600

    Add files via upload

---
+++ EXAMEN_1RA_EVALUACION_TEST_COVID/src/examen_1ra_evaluacion_test_covid/EXAMEN_1RA_EVALUACION_TEST_COVID.java
@@ -0,0 +1,13 @@
+
+package examen_1ra_evaluacion_test_covid;
+
+
+public class EXAMEN_1RA_EVALUACION_TEST_COVID {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
---
+++ EXAMEN_1RA_EVALUACION_TEST_COVID/src/examen_1ra_evaluacion_test_covid/TestCovid.java
@@ -0,0 +1,31 @@
+
+package examen_1ra_evaluacion_test_covid;
+
+public class TestCovid {
+ private int edad;
+ private boolean Ecronica;
+ private double peso;
+ private double estatura;
+
+ 
+ public TestCovid(){
+     edad = 70;
+     Ecronica = true; //No tiene una enfermedad crónica
+     peso = 56.8;
+     estatura = 1.75;
+    
+ }
+ 
+ public void calcularPersonaRiesgo(){
+     
+     if ((edad >= 65) OR (Ecronica = true) OR (imc > 30)){
+     
+ }
+  
+ 
+ }
+ private void calcularIMC(){
+     double IMC;
+     IMC = peso / estatura;
+ }
+}
